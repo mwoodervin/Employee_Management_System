68f20e3c5a7abb8f23579bd7463e1435aff07886
Mary Wood <mwoodervin@gmail.com>
2020-09-08 11:55:56 -0400

    connected to database; initial inquirer prompts

--- app.js
+++ app.js
@@ -2,3 +2,50 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 const consoleTable = require("console.table");
 
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "root12345",
+    database: "Employee_Management"
+  });
+  
+function start() {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "action",
+            message: "What would you like to do?",
+            choices: ["VIEW employees by manager", "UPDATE employee managers","DELETE departments"]
+        },
+        {
+            type: "list",
+            name: "view",
+            message: "Select a manager",
+            choices: [{
+                name: "Mary",
+                value: "Manager", 
+                short: "MWE"}, 
+                {
+                name: "Miriam", 
+                value: "Manager", 
+                short: "MRE"}],
+            when: (answers) => answers.action === "VIEW employees by manager"
+        }
+    ]);
+}
+
+  // connect to the mysql server and sql database
+  connection.connect(function(err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
+  });
+  

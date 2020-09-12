9eb3887a4500bdd7b6d6805aa00e8a3b4b260709
Mary Wood <mwoodervin@gmail.com>
2020-09-12 17:57:49 -0400

    Populated seed file with database content

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ function viewDepartments() {
 }
 
 function viewRoles() {
-    const query = "SELECT role.id '', role.title, role.salary, department.name deptname FROM role LEFT JOIN department ON role.department_id = department.id";
+    const query = "SELECT roles.id '', roles.title, roles.salary, department.dept_name deptname FROM roles LEFT JOIN department ON roles.department_id = department.id";
     connection.query(query, function (err, results) {
         if (err) throw err;
         console.table(results);
@@ -72,7 +72,7 @@ function viewRoles() {
 }
 
 function viewEmployees() {
-    const query = "SELECT employee.id, concat(employee.first_name, ' ', employee.last_name) employee, role.title role FROM employee LEFT JOIN role ON employee.role_id = role.id";
+    const query = "SELECT employee.id, concat(employee.first_name, ' ', employee.last_name) employee, roles.title role FROM employee LEFT JOIN roles ON employee.role_id = role.id";
     connection.query(query, function (err, results) {
         if (err) throw err;
         console.table(results);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,36 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+-- populate database with starter content
+
+
+-- Departments
+INSERT INTO department (dept_name) VALUES ('Finance');
+INSERT INTO department (dept_name) VALUES ('Sales/Marketing');
+INSERT INTO department (dept_name) VALUES ('Engineering/Development');
+INSERT INTO department (dept_name) VALUES ('Customer Support');
+INSERT INTO department (dept_name) VALUES ('Legal');
+
+
+-- Roles
+INSERT INTO roles (title, salary, department_id) VALUES ('Sales Team Manager', 160000, 2);
+INSERT INTO roles (title, salary, department_id) VALUES ('House Counsel', 180000, 5);
+INSERT INTO roles (title, salary, department_id) VALUES ('Paralegal', 100000, 5);
+INSERT INTO roles (title, salary, department_id) VALUES ('Lead Developer', 150000, 3);
+INSERT INTO roles (title, salary, department_id) VALUES ('Software Engineer', 80000, 3);
+INSERT INTO roles (title, salary, department_id) VALUES ('Salesperson', 80000, 2);
+INSERT INTO roles (title, salary, department_id) VALUES ('Accountant', 150000, 1);
+INSERT INTO roles (title, salary, department_id) VALUES ('CFO', 100000, 1);
+INSERT INTO roles (title, salary, department_id) VALUES ('Customer Support Manager', 160000, 4);
+INSERT INTO roles (title, salary, department_id) VALUES ('Customer Liaison', 70000, 4);
+
+-- Employees
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Jim', 'Jones',1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Lucy', 'Loo',2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Mary', 'Lamb',3 ,2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Anna', 'Banana',4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Frank', 'Beans',5 ,4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Sam', 'Iam',6 ,1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Sarah', 'Franklin',7,8);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Abraham', 'Lincoln',8);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Liz', 'Lemon',9);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Super', 'Mann',10 ,9);
+
+

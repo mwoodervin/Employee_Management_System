15508c58700fe02257010f4c6b9f6b6e62648716
Mary Wood <mwoodervin@gmail.com>
2020-09-13 18:54:21 -0400

    fixed error in seeds.sql file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,6 @@
--- populate database with starter content
+-- populate database with Departments, Roles and Employees
 
 
--- Departments
 INSERT INTO department (dept_name) VALUES ('Finance');
 INSERT INTO department (dept_name) VALUES ('Sales/Marketing');
 INSERT INTO department (dept_name) VALUES ('Engineering/Development');
@@ -9,7 +8,6 @@ INSERT INTO department (dept_name) VALUES ('Customer Support');
 INSERT INTO department (dept_name) VALUES ('Legal');
 
 
--- Roles
 INSERT INTO roles (title, salary, department_id) VALUES ('Sales Team Manager', 160000, 2);
 INSERT INTO roles (title, salary, department_id) VALUES ('House Counsel', 180000, 5);
 INSERT INTO roles (title, salary, department_id) VALUES ('Paralegal', 100000, 5);
@@ -21,12 +19,11 @@ INSERT INTO roles (title, salary, department_id) VALUES ('CFO', 180000, 1);
 INSERT INTO roles (title, salary, department_id) VALUES ('Customer Support Manager', 160000, 4);
 INSERT INTO roles (title, salary, department_id) VALUES ('Customer Liaison', 60000, 4);
 
--- Employees
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Jim', 'Jones',1, null);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Lucy', 'Loo',2, null);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Mary', 'Lamb',3 ,2);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Anna', 'Banana',4, null);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Frank', 'Beans',5 ,4, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Frank', 'Beans',5 ,4);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Sam', 'Iam',6 ,1);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Sarah', 'Franklin',7,8);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Abraham', 'Lincoln',8, null);
